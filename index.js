7964a5d604aa3eec9b2d65a52915b07f925f517c
jl33-ai <justinkhlee27@gmail.com>
2024-07-17 21:03:25 +1000

    Can augment front matter (fucking finally)

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 const ipfsUploader = require('./lib/ipfs-uploader');
 const config = require('./lib/config');
-const { streamToString } = require('./lib/helpers');
+const {streamToString} = require('./lib/helpers');
+const fs = require('fs');
 
 const pluginConfig = config(hexo);
 
-hexo.extend.filter.register('after_generate', async function() {
-    const { route } = this;
+hexo.extend.filter.register('after_generate', async function () {
+    const {route} = this;
 
     try {
-        await ipfsUploader.initializeIPFS();
+        await ipfsUploader.initializeIPFS(pluginConfig);
+        const posts = hexo.locals.get('posts');
 
-        const routes = route.list();
-        hexo.log.info("Surely not")
-        const cid = await ipfsUploader.uploadToIPFS("Hi world");
-        hexo.log.info(cid)
-        for (const path of routes) {
-            if (path.endsWith('.html')) {
-        //         const content = route.get(path);
-        //         const htmlContent = await streamToString(content);
-        //
-        //         // Upload to IPFS
-        //         const cid = await ipfsUploader.uploadToIPFS(htmlContent);
-        //
-        //         // Update the page's front-matter
-        //         const page = hexo.locals.get('pages').find(p => p.path === path) ||
-        //             hexo.locals.get('posts').find(p => p.path === path);
-        //
-        //         if (page) {
-        //             page.ipfs = {
-        //                 cid: cid,
-        //                 timestamp: Date.now(),
-        //                 version: (page.ipfs && page.ipfs.version) ? page.ipfs.version + 1 : 1
-        //             };
-        //         }
-        //
-        //         hexo.log.info(`Uploaded ${path} to IPFS with CID: ${cid}`);
+        for (const post of posts.data) {
+            if (post && post.ipfs === true) {
+                const postPath = post.path;
+                hexo.log.info(`Attempting to upload ${postPath}`)
+                const htmlPath = postPath.replace(/\.md$/, '.html');
+                const htmlContent = await getRouteContent(route, htmlPath);
+
+                if (htmlContent) {
+                    const cid = await ipfsUploader.uploadToIPFS(htmlContent);
+
+                    if (!post.cids) {
+                        post.cids = [];
+                    }
+                    post.cids.unshift(cid);
+
+                    const sourceFilePath = post.full_source;
+                    let postContent = fs.readFileSync(sourceFilePath, 'utf8');
+                    const cidsRegex = /cids:(\s*- .*$)*/m;
+                    const newCidsSection = `cids:\n  - ${cid}${post.cids.slice(1).map(c => `\n  - ${c}`).join('')}`;
+
+                    if (cidsRegex.test(postContent)) {
+                        postContent = postContent.replace(cidsRegex, newCidsSection);
+                    } else {
+                        const frontMatterEnd = postContent.indexOf('---', postContent.indexOf('---') + 1);
+                        postContent = postContent.slice(0, frontMatterEnd) + `${newCidsSection}\n` + postContent.slice(frontMatterEnd);
+                    }
+                    fs.writeFileSync(sourceFilePath, postContent);
+
+                    hexo.log.info(`Uploaded ${post.title} to IPFS with CID: ${cid}`);
+                } else {
+                    hexo.log.warn(`Could not find HTML content for post: ${post.title}`);
+                }
             }
         }
     } catch (error) {
@@ -42,12 +51,32 @@ hexo.extend.filter.register('after_generate', async function() {
     }
 });
 
+async function getRouteContent(route, path) {
+    return new Promise((resolve, reject) => {
+        const stream = route.get(path);
+        if (!stream) {
+            resolve(null);
+            return;
+        }
+        let content = '';
+        stream.on('data', chunk => (content += chunk));
+        stream.on('end', () => resolve(content));
+        stream.on('error', reject);
+    });
+}
 
-// hexo.extend.helper.register('getIPFSLink', function(page) {
-//     if (page.ipfs && page.ipfs.cid) {
-//         return `https://ipfs.io/ipfs/${page.ipfs.cid}`;
-//     }
-//     return '';
-// });
+hexo.extend.helper.register('getLatestIpfsCID', function (page) {
+    if (page.cids && page.cids.length > 0) {
+        return page.cids[0];
+    }
+    return null
+})
+
+hexo.extend.helper.register('getIpfsLink', function (page) {
+    if (page.ipfs && page.ipfs.cid) {
+        return `https://ipfs.io/ipfs/${page.ipfs.cid}`;
+    }
+    return '';
+});
 
 
